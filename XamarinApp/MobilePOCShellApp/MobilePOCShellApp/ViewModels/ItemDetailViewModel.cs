f75161c4525497f1fdd8eb001342938988eaca63
Fernando Martinez <39782229+FernandoAMartinez@users.noreply.github.com>
2020-04-10 02:50:14 -0300

    Initial Xamarin.Forms App Layout

---
+++ XamarinApp/MobilePOCShellApp/MobilePOCShellApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MobilePOCShellApp.Models;
+
+namespace MobilePOCShellApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
